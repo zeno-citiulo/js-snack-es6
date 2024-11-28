21e0b199d414eddef11a0e2d8e81cbabeeabb27a
zeno-citiulo <zenocit02@gmail.com>
2024-11-28 14:35:11 +0100

    feat: first exercise completed

--- snack 1/script.js
+++ snack 1/script.js
@@ -10,35 +10,47 @@
 const bikes = [
   {
     nome: 'Specialized Tarmac SL7',
-    peso:  6,8 , 
-  }
+    peso:  6.8
+  },
   {
     nome: 'Canyon Ultimate CFR',
-    peso:  6,2 ,
-  }
+    peso:  6.2
+  },
   {
     nome: 'Pinarello Dogma F',
-    peso:  7,6 ,
-  }
+    peso:  7.6 
+  },
   {
     nome: 'Trek Emonda SLR',
-    peso:  8,6 , 
-  }
+    peso:  8.6 
+  },
   {
     nome: 'BMC Teammachine SLR01',
-    peso:  5,9 ,
-  }
+    peso:  5.9
+  },
   {
     nome: 'Giant TCR Advanced SL Disc',
-    peso:  6,0 ,
-  }
+    peso:  6.0
+  },
   {
     nome: 'Scott Addict RC Ultimate',
-    peso:  9,2 ,
-  }
+    peso:  9.2
+  },
   {
     nome: 'SColnago V4Rs',
-    peso:  9,0 ,
+    peso:  9.0
   }
 ]
 
+let lighterBike = bikes[0]
+console.log(lighterBike)
+
+for(let i = 0; i < bikes.length; i++){
+  const bike = bikes[i];
+  if(bike.peso < lighterBike.peso){
+    lighterBike = bike;
+  }
+}
+
+console.log(lighterBike)
+document.getElementById('risultato').innerHTML =  `La bici più leggera di tutte è la bici ${lighterBike.nome}, con il peso di ${lighterBike.peso} ´
