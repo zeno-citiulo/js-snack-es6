f2c705abc88cbb86952e14f56bac6dfa9c7d38a5
zeno-citiulo <zenocit02@gmail.com>
2024-11-28 14:28:21 +0100

    feat: add in js array of object

---
+++ snack 1/script.js
@@ -0,0 +1,44 @@
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+//Creo un array di oggetti(bici)
+//Ogni oggetto descriverà una bici
+//Ogni bici dovrà avere le proprietà nome e peso
+//Alla fine dovrà stampare a schermo la bici con il peso minone fra tutte
+
+const bikes = [
+  {
+    nome: 'Specialized Tarmac SL7',
+    peso: '6,8 '
+  }
+  {
+    nome: 'Canyon Ultimate CFR',
+    peso: '6,2 '
+  }
+  {
+    nome: 'Pinarello Dogma F',
+    peso: '7,6 '
+  }
+  {
+    nome: 'Trek Emonda SLR',
+    peso: '8,6 '
+  }
+  {
+    nome: 'BMC Teammachine SLR01',
+    peso: '5,9 '
+  }
+  {
+    nome: 'Giant TCR Advanced SL Disc',
+    peso: '6,0 '
+  }
+  {
+    nome: 'Scott Addict RC Ultimate',
+    peso: '9,2 '
+  }
+  {
+    nome: 'SColnago V4Rs',
+    peso: '9,0 '
+  }
+]
+
